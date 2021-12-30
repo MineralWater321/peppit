b86ede2ab9cfb94a1977e528fe2e9fb2fab2f9a3
hinzwifi <markjoshuahaynes@protonmail.com>
2021-12-30 10:17:55 +0800

    Added Routing and Worked on About

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return(
-        <nav className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+        <nav className="navbar  shadow-lg bg-neutral text-neutral-content  ">
             <div className="flex-none hidden lg:flex">
                 <button className="btn btn-square btn-ghost">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">           
--- src/page/About.jsx
+++ src/page/About.jsx
@@ -5,9 +5,34 @@ function About() {
     return (
         <>
         <Navbar />
-        <div>
-            About
-        </div>
+        
+<div class="my-0 hero-first hero min-h-screen bg-base-200">
+  <div class="flex-col hero-content lg:flex-row-reverse">
+    <img src="https://picsum.photos/id/1005/600/600" class="max-w-sm rounded-lg shadow-2xl" /> 
+    <div>
+      <h1 class="mb-5 text-5xl font-bold">
+            Hello there
+          </h1> 
+      <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p> 
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+<div class="hero min-h-screen bg-base-200">
+  <div class="text-center hero-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">
+            Hello there
+          </h1> 
+      <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p> 
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
         </>
     )
 }
