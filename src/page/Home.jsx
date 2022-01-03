b9082b923849d9313b13e49d805ff85c6a96bbd2
hinzwifi <markjoshuahaynes@protonmail.com>
2022-01-03 22:29:24 +0800

    Added Pre-post

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,76 +1,119 @@
-import React, {useEffect, useState} from 'react'
-import Navbar from '../components/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { RiContrast2Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 // import { Check } from '../Auth'
 // import { yoink } from '../Auth'
-
+import { BiUpvote, BiDownvote } from "react-icons/bi";
 function Home() {
-//     console.log(yoink)
-//     const [userName, setuserName] = useState("")
-//    useEffect(() => {
-//     async function msg() {
-//         const something = await Check()
-//         await setuserName(something);
-//       }
-//       msg();
-//    }, [userName])
-     
-    return (
-        <>
-            {/* Make a card */}
-            <div className=" max-w-3xl mx-auto" >
-                <div className="post bg-red-300 my-3 rounded-md p-10">
-                    <div className="post-header">
-                    <p class="post_header">
-		            <a class="post_subreddit" href="/r/AskReddit">r/AskReddit</a>
-		<span class="dot">•</span>
-		<a class="post_author" href="/u/blue_pookie">u/blue_pookie</a>
-		<span class="dot">•</span>
-		<span class="created" title="Jan 02 2022, 17:10:21 UTC">20h ago</span>
-		
-			
-			<span class="award" title="Wholesome Seal of Approval">
-				<img alt="Wholesome Seal of Approval" src="/preview/pre/award_images/t5_22cerq/b9ks3a5k7jj41_WholesomeSealofApproval.png?width=16&amp;height=16&amp;auto=webp&amp;s=1a331be5cf6d754b4cb7ed2ca3706f70d5260a57" width="16" height="16" />
-			</span>
-			
-			<span class="award" title="Silver">
-				<img alt="Silver" src="/static/gold/awards/icon/silver_16.png" width="16" height="16" />
-			</span>
-			
-			<span class="award" title="Gold">
-				<img alt="Gold" src="/static/gold/awards/icon/gold_16.png" width="16" height="16" />
-			</span>
-			 
-			<span class="award" title="Helpful">
-				<img alt="Helpful" src="/preview/pre/award_images/t5_22cerq/klvxk1wggfd41_Helpful.png?width=16&amp;height=16&amp;auto=webp&amp;s=a5662dfbdb402bf67866c050aa76c31c147c2f45" width="16" height="16" />
-			</span>
-			
-			<span class="award" title="Wholesome">
-				<img alt="Wholesome" src="/preview/pre/award_images/t5_22cerq/5izbv4fn0md41_Wholesome.png?width=16&amp;height=16&amp;auto=webp&amp;s=92932f465d58e4c16b12b6eac4ca07d27e3d11c0" width="16" height="16" />
-			</span>
-			
-			<span class="award" title="Take My Energy">
-				<img alt="Take My Energy" src="/preview/pre/award_images/t5_q0gj4/p4yzxkaed5f61_oldtakemyenergy.png?width=16&amp;height=16&amp;auto=webp&amp;s=10034f3fdf8214c8377134bb60c5b832d4bbf588" width="16" height="16" />
-			</span>
-			
-		
-	</p>
-                    </div>
-                    <div className="post-title">
+  //     console.log(yoink)
+  //     const [userName, setuserName] = useState("")
+  //    useEffect(() => {
+  //     async function msg() {
+  //         const something = await Check()
+  //         await setuserName(something);
+  //       }
+  //       msg();
+  //    }, [userName])
 
-                    </div>
-                    <div className="divider">
-                  
-                    </div>
-                    <div className="post-footer">
-              
-                    </div>
+  return (
+    <>
+      {/* Make Link card */}
+      <div className=" max-w-3xl mx-auto">
+        <div className="post bg-primary  my-3 rounded-lg p-10">
+          <div className="post-header flex flex-wrap">
+            <Link
+              class="hover:text-neutral hover:underline post_subreddit"
+              to="/r/AskReddit"
+            >
+              r/AskReddit
+            </Link>
+            <span class="mx-3 dot">•</span>
+            <Link
+              class="hover:text-neutral hover:underline post_author"
+              to="/u/blue_pookie"
+            >
+              u/blue_pookie
+            </Link>
+            <span class="mx-3 dot">•</span>
+            <span class="created" title="Jan 02 2022, 17:10:21 UTC">
+              20h ago
+            </span>
+
+            <span class="award" title="Wholesome Seal of Approval">
+              <RiContrast2Fill size="16" />
+            </span>
+
+            <span class="award" title="Silver">
+              <RiContrast2Fill size="16" />
+            </span>
+
+            <span class="award" title="Gold">
+              <RiContrast2Fill size="16" />
+            </span>
+
+            <span class="award" title="Helpful">
+              <RiContrast2Fill size="16" />
+            </span>
+
+            <span class="award" title="Wholesome">
+              <RiContrast2Fill size="16" />
+            </span>
+
+            <span class="award" title="Take My Energy">
+              <RiContrast2Fill size="16" />
+            </span>
+          </div>
+          <div className=" font-bold text-lg md:text-2xl post-title">
+            <Link to="/r/AskReddit/comments/rue32w/without_naming_it_what_is_a_tv_show_that_is/">
+              Without naming it, what is a TV show that is instantly
+              recognizable based on one quote?
+            </Link>
+          </div>
+          <div className="divider"></div>
+          <div className="flex post-votes max-w-max">
+            <button className="m-auto mx-2">
+              <BiUpvote size="16" />
+            </button>
+            <div>69</div>
+            <button className="m-auto mx-2">
+              <BiDownvote size="16" />
+            </button>
+          </div>
+          {/* <div className="post-footer">
+            <div>
+              <div>
+                <a
+                  rel="nofollow"
+                  data-click-id="comments"
+                  data-test-id="comments-page-link-num-comments"
+                  href="/r/Philippines/comments/rukhy3/daily_random_discussion_jan_03_2022/"
+                >
+                  <span>535 comments</span>
+                </a>
+                <div id="t3_rukhy3-share-menu">
+                  <button data-click-id="share">
+                    <span>share</span>
+                  </button>
                 </div>
-              <div className=" bg-red-300 my-3 rounded-md p-10">d</div>
-              <div className=" bg-red-300 my-3 rounded-md p-10">d</div>
-              <div className=" bg-red-300 my-3 rounded-md p-10">d</div>
+                <div data-ignore-click="false">
+                  <button>
+                    <span></span>
+                    <span>save</span>
+                  </button>
+                </div>
+              </div>
             </div>
-        </>
-    )
+          </div> */}
+        </div>
+        <div className=" bg-primary text-bg-primary-content my-3 rounded-md p-10">
+          d
+        </div>
+        <div className=" bg-primary my-3 rounded-md p-10">d</div>
+        <div className=" bg-primary my-3 rounded-md p-10">d</div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
