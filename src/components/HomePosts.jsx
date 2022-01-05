23275802a73705a55adb252fb44653fc47158c8b
hinzwifi <markjoshuahaynes@protonmail.com>
2022-01-05 22:38:01 +0800

    Added an Icon to the Project and Added New Styles

--- src/components/HomePosts.jsx
+++ src/components/HomePosts.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-import Navbar from "../components/Navbar";
+import React, { useEffect, useState, useRef } from "react";
+
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 import {
   RiContrast2Fill,
   RiArrowUpCircleFill,
@@ -10,7 +13,9 @@ import {
 import { Link } from "react-router-dom";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { FaShare, FaComments, FaSave } from "react-icons/fa";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 function HomePosts() {
+  const [loading, setLoading] = useState(true);
   const [upvotes, setupvotes] = useState(69);
   const [voted, setvoted] = useState(true);
   const [arrowUp, setarrowUp] = useState(true);
@@ -41,9 +46,13 @@ function HomePosts() {
       <>{arrowDown ? <RiArrowDownCircleLine /> : <RiArrowDownCircleFill />}</>
     );
   }
+
+  const imageLoaded = () => {
+    setLoading(false);
+  };
   return (
     <>
-      <div className="post bg-primary  my-3 rounded-lg p-2 md:p-5">
+      <div className="post bg-primary  my-3 rounded-lg p-2 md:p-5  mx-3 md:mx-0">
         <div className="post-header flex flex-wrap">
           <Link
             class="hover:text-neutral hover:underline post_subreddit"
@@ -87,23 +96,40 @@ function HomePosts() {
             <RiContrast2Fill size="16" />
           </span>
         </div>
-        <div className=" font-bold text-md md:text-lg post-data-tip">
+        <div className=" font-bold text-sm md:text-md md:text-lg post-data-tip">
           <Link to="/r/AskReddit/without_naming_it_what_is_a_tv_show_that_is/">
-            Without naming it, what is a TV show that is instantly recognizable
-            based on one quote?
+            BREAKING: New COVID-19 infections nationwide shoot up to 10,775 –
+            nearly double yesterday’s count, bringing the tally to 2,871,745,
+            the Department of Health reports. The positivity rate stands at
+            31.7%, which means almost 1 in 3 people tested for COVID-19 turned
+            out infected. https://t.co/qcu7Kd6HR
           </Link>
         </div>
         <div className="flex justify-center  my-3">
-          <img
-            className="post-image"
+          {/* <div className={` ${loading ? "" : "hidden"}   `}>
+            <LazyLoadImage alt="Post" src={kekw} />
+            <span className="text-3xl my-auto">Loading...</span>
+          </div> */}
+          <div
+            className={` ${
+              loading ? "" : "hidden"
+            } flex items-center justify-center   `}
+          >
+            <div class="animate-spin ">
+              <AiOutlineLoading3Quarters />
+            </div>
+          </div>
+          <LazyLoadImage
+            className={`post-image skeleton  ${loading ? "hidden" : " "}  `}
             alt="Post"
-            src="https://libredd.it/preview/external-pre/UQxlAGYbxFWQA1r1-JUp-XQs4VWlzyBPn-g81HaX2OI.jpg?auto=webp&s=ca0bca9142e93715ea6777294eb36fa62bc9c579"
+            src="https://memesboy.com/wp-content/uploads/2018/03/Is-It-Gold-In-Here-Or-Is-It-Gold-Meme.jpg"
+            onLoad={imageLoaded}
           />
         </div>
         {/* <a href="/r/Philippines/comments/rv5rrp/hindi_pa_talaga_nadala_ang_pilipinas_sa/">
           <div className="flex justify-center">
             <div>
-              <img
+              <LazyLoadImage
                 alt="Post"
                 className=" sm:max-w-sm w-auto"
                 src="https://libredd.it/preview/external-pre/UQxlAGYbxFWQA1r1-JUp-XQs4VWlzyBPn-g81HaX2OI.jpg?auto=webp&s=ca0bca9142e93715ea6777294eb36fa62bc9c579"
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,6 +1,7 @@
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import picture from "../assets/NEW.png";
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -14,58 +15,67 @@ export default function Login() {
 
   return (
     <>
-      <div className="flex justify-center items-center h-screen login-page ">
-        <form onSubmit={submitForm} className=" w-8/12 lg:w-6/12  ">
-          <div className="form-control ">
-            <label className="label">
-              <span className="label-text text-3xl">Username</span>
-            </label>
-            <input
-              autoComplete="yes"
-              type="text"
-              placeholder="USERNAME"
-              className="input input-bordered "
-              value={username}
-              required
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="form-control  ">
-            <label className="label ">
-              <span className="label-text text-3xl">Password</span>
-            </label>
+      <div className="login-page">
+        <div className="flex justify-center items-center h-screen form-frosty  ">
+          <form onSubmit={submitForm} className=" w-8/12 lg:w-4/12  ">
+            <div className=" text-center text-6xl md:text-9xl">
+              P
+              <Link to="/" className=" text-red-400 link">
+                eppit
+              </Link>
+            </div>
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text text-3xl">Username</span>
+              </label>
+              <input
+                autoComplete="yes"
+                type="text"
+                placeholder="Username"
+                className="input input-bordered "
+                value={username}
+                required
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-control  ">
+              <label className="label ">
+                <span className="label-text text-3xl">Password</span>
+              </label>
 
-            <input
-              type="password"
-              title="Enter an ID consisting of 4-8 hexadecimal digits"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              aria-autocomplete="list"
-              autoComplete="current-password"
-              placeholder="PASSWORD"
-              className="input input-bordered  "
-              minLength="8"
-              required
-            />
-          </div>
-          <div class="divider"></div>
-          <div class="form-control ">
-            <button
-              class="btn btn-primary btn-active my-3"
-              aria-pressed="true"
-              type="submit"
-            >
-              Submit
-            </button>
-            <Link
-              class="btn btn-error btn-active  my-3"
-              aria-pressed="true"
-              to="/"
-            >
-              Cancel
-            </Link>
-          </div>
-        </form>
+              <input
+                type="password"
+                title="Enter an ID consisting of 4-8 hexadecimal digits"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                aria-autocomplete="list"
+                autoComplete="current-password"
+                placeholder="Password"
+                className="input input-bordered  "
+                minLength="8"
+                required
+              />
+            </div>
+            <div class="divider"></div>
+            <div class="form-control ">
+              <button
+                class="btn btn-primary btn-active my-3"
+                aria-pressed="true"
+                type="submit"
+              >
+                Login
+              </button>
+              <Link
+                class="btn btn-error btn-active  my-3"
+                aria-pressed="true"
+                to="/"
+              >
+                Cancel
+              </Link>
+            </div>
+          </form>
+        </div>
+        {/* <footer className="bg-red-300 ">BRUH</footer> */}
       </div>
     </>
   );
